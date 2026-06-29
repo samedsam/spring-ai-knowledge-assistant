acd51569dc4640042b0bf6f8df3aecabb52d14d0
Mehraz Abdessamad <samed3701@hotmail.fr>
2026-06-29 14:59:41 +0200

    Initialize Spring Boot and Spring AI project

---
+++ src/main/java/com/smartgnt/knowledgeassistant/SpringAiKnowledgeAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.smartgnt.knowledgeassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiKnowledgeAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAiKnowledgeAssistantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartgnt/knowledgeassistant/SpringAiKnowledgeAssistantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartgnt.knowledgeassistant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiKnowledgeAssistantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
